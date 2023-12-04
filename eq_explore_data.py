95440a7bf85ef78e7440bce275ba761e6f8ad3b7
Drpunctual0 <topciruveyda@gmail.com>
2023-12-04 06:35:04 +0300

    Update eq_explore_data.py
    updates

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -11,11 +11,6 @@ filename = 'data2/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-# readable_file = 'data2/readable_eq_data.json'
-
-# with open(readable_file, 'w') as f:
-#     json.dump(all_eq_data, f, indent=4)
-
 
 all_eq_dicts = all_eq_data['features']
 # print(len(all_eq_dicts))
